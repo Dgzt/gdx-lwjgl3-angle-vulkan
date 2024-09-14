8183c0b1741b2a2b743aa05d3be97fcbcd018669
Zsuró Tibor <zsurotibor@gmail.com>
2024-09-14 22:02:04 +0200

    Copied lwjgl3 files from libgdx repo

---
+++ src/main/java/com/github/dgzt/gdx/lwjgl3/angle/vulkan/Lwjgl3ApplicationBase.java
@@ -0,0 +1,13 @@
+
+package com.github.dgzt.gdx.lwjgl3.angle.vulkan;
+
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Input;
+import com.badlogic.gdx.backends.lwjgl3.audio.Lwjgl3Audio;
+
+public interface Lwjgl3ApplicationBase extends Application {
+
+    Lwjgl3Audio createAudio (Lwjgl3ApplicationConfiguration config);
+
+    Lwjgl3Input createInput (Lwjgl3Window window);
+}

5eb49a017e8c644d6d58c1ad81bed1c84bfdbf66
Zsuró Tibor <zsurotibor@gmail.com>
2025-11-10 16:41:23 +0100

    NPE fix in initializeGlfw

--- src/main/java/com/github/dgzt/gdx/lwjgl3/Lwjgl3VulkanApplication.java
+++ src/main/java/com/github/dgzt/gdx/lwjgl3/Lwjgl3VulkanApplication.java
@@ -87,13 +87,15 @@ public class Lwjgl3VulkanApplication implements Lwjgl3ApplicationBase {
             Lwjgl3NativesLoader.load();
             errorCallback = GLFWErrorCallback.createPrint(Lwjgl3ApplicationConfiguration.errorStream);
             GLFW.glfwSetErrorCallback(errorCallback);
-            switch (ANGLELoader.angleBackend) {
-                case DIRECT3D_9: GLFW.glfwInitHint(GLFW.GLFW_ANGLE_PLATFORM_TYPE, GLFW.GLFW_ANGLE_PLATFORM_TYPE_D3D9); break;
-                case DIRECT3D_11: GLFW.glfwInitHint(GLFW.GLFW_ANGLE_PLATFORM_TYPE, GLFW.GLFW_ANGLE_PLATFORM_TYPE_D3D11); break;
-                case DESKTOP_GL: GLFW.glfwInitHint(GLFW.GLFW_ANGLE_PLATFORM_TYPE, GLFW.GLFW_ANGLE_PLATFORM_TYPE_OPENGL); break;
-                case GL_ES: GLFW.glfwInitHint(GLFW.GLFW_ANGLE_PLATFORM_TYPE, GLFW.GLFW_ANGLE_PLATFORM_TYPE_OPENGLES); break;
-                case VULKAN: GLFW.glfwInitHint(GLFW.GLFW_ANGLE_PLATFORM_TYPE, GLFW.GLFW_ANGLE_PLATFORM_TYPE_VULKAN); break;
-                case METAL: GLFW.glfwInitHint(GLFW.GLFW_ANGLE_PLATFORM_TYPE, GLFW.GLFW_ANGLE_PLATFORM_TYPE_METAL); break;
+            if (ANGLELoader.angleBackend != null) {
+                switch (ANGLELoader.angleBackend) {
+                    case DIRECT3D_9: GLFW.glfwInitHint(GLFW.GLFW_ANGLE_PLATFORM_TYPE, GLFW.GLFW_ANGLE_PLATFORM_TYPE_D3D9); break;
+                    case DIRECT3D_11: GLFW.glfwInitHint(GLFW.GLFW_ANGLE_PLATFORM_TYPE, GLFW.GLFW_ANGLE_PLATFORM_TYPE_D3D11); break;
+                    case DESKTOP_GL: GLFW.glfwInitHint(GLFW.GLFW_ANGLE_PLATFORM_TYPE, GLFW.GLFW_ANGLE_PLATFORM_TYPE_OPENGL); break;
+                    case GL_ES: GLFW.glfwInitHint(GLFW.GLFW_ANGLE_PLATFORM_TYPE, GLFW.GLFW_ANGLE_PLATFORM_TYPE_OPENGLES); break;
+                    case VULKAN: GLFW.glfwInitHint(GLFW.GLFW_ANGLE_PLATFORM_TYPE, GLFW.GLFW_ANGLE_PLATFORM_TYPE_VULKAN); break;
+                    case METAL: GLFW.glfwInitHint(GLFW.GLFW_ANGLE_PLATFORM_TYPE, GLFW.GLFW_ANGLE_PLATFORM_TYPE_METAL); break;
+                }
             }
             GLFW.glfwInitHint(GLFW.GLFW_JOYSTICK_HAT_BUTTONS, GLFW.GLFW_FALSE);
             if (!GLFW.glfwInit()) {
